fcec4011487f03de423ea343e8297705938600b3
Mohammad Shokri Khanghah <shokri.md@gmail.com>
2024-02-22 17:08:34 +0330

    Tweak CF analytics configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,13 +2,13 @@
 export default defineNuxtConfig({
     devtools: { enabled: true },
     runtimeConfig: {
-        cloudflareAnalytics: {
-            token: process.env.NUXT_CF_ANALYTICS_TOKEN,
-        }
     },
     modules: [
         [
             'nuxt-cloudflare-analytics',
         ],
-    ]
+    ],
+    cloudflareAnalytics: {
+        token: process.env.NUXT_CF_ANALYTICS_TOKEN,
+    }
 })
