a9e7827a3d4b2d1296d2f42ccf3214ef5654c760
Mohammad Shokri Khanghah <shokri.md@gmail.com>
2024-02-22 16:15:56 +0330

    Support CF analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+    devtools: { enabled: true },
+    runtimeConfig: {
+        cfAnalyticsToken: ''
+    },
+    modules: [
+        [
+            'nuxt-cloudflare-analytics',
+            {
+                token: '${CF_ANALYTICS_TOKEN}',
+            },
+        ],
+    ]
 })
