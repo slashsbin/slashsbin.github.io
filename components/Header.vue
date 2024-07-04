904d8f3e713894270da2c996c5b5b74f28bb150b
Mohammad Shokri Khanghah <shokri.md@gmail.com>
2024-07-04 13:30:29 +0330

    Improve SEO

--- components/Header.vue
+++ components/Header.vue
@@ -1,3 +1,12 @@
 <template>
-    <div></div>
+    <div>
+        <div class="sr-only">
+            <h1 class="sr-only" lang="en-US">{{ appConfig.author.name }}'s personal website</h1>
+            <h1 class="sr-only" lang="fa-IR">وب سایت شخصی {{ appConfig.author.altName }}</h1>
+        </div>
+    </div>
 </template>
+
+<script setup lang="ts">
+    const appConfig = useAppConfig();
+</script>
