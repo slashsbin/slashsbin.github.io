b26597f6694a3c73305582c04b13c8f5cee0e7c8
Mohammad Shokri Khanghah <shokri.md@gmail.com>
2024-07-04 20:09:08 +0330

    WIP: CyberRoze component

---
+++ components/CyberRoze.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+    useHead({
+        bodyAttrs: {
+            id: '!cyberRoze.md',
+            class: 'group/cyberRoze',
+        },
+    });
+</script>
+
+<style>
+    /* #\!cyberRoze\.md {} */
+
+    pre {
+        @apply group-target/cyberRoze:font-black;
+    }
+</style>
