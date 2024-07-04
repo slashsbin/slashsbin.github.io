80a45a6574351846766216f33f1b312883d5f939
Mohammad Shokri Khanghah <shokri.md@gmail.com>
2024-07-04 16:44:01 +0330

    Introduce header companion

--- components/Header.vue
+++ components/Header.vue
@@ -1,5 +1,6 @@
 <template>
     <div>
+        <div class="h-px bg-gradient-to-r from-persian-green to-persian-rose"></div>
         <div class="sr-only">
             <h1 class="sr-only" lang="en-US">{{ appConfig.author.name }}'s personal website</h1>
             <h1 class="sr-only" lang="fa-IR">وب سایت شخصی {{ appConfig.author.altName }}</h1>
