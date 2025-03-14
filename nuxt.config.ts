199b61552e6ecfb33a02e2b87cb45a93473aa642
Mohammad Shokri Khanghah <shokri.md@gmail.com>
2025-03-14 12:09:29 +0330

    Migrate SEO module from RC to Major release

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,41 +1,46 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    devtools: { enabled: true },
-    telemetry: false,
-    modules: [
-        // Cloudflare Analytics
-        [
-            'nuxt-cloudflare-analytics',
-            {
-                token: '07684a2c00274afeac7127bec28c3e3d',
-            }
-        ],
-        // SEO
-        '@nuxtjs/seo',
-        // TailwindCSS
-        [
-            '@nuxtjs/tailwindcss',
-            {
-                exposeConfig: true,
-            }
-        ],
-        // G Analytics
-        [
-            'nuxt-gtag',
-            {
-                id: 'G-1WY2RCYLML'
-            }
-        ],
-    ],
-    site: {
-        name: '/sβin.dev',
-        url: 'https://slashsbin.dev'
-    },
-    robots: {
-        credits: false,
-    },
-    sitemap: {
-        autoLastmod: true,
-        credits: false,
-    },
+  devtools: { enabled: true },
+  telemetry: false,
+  compatibilityDate: '2025-01-01',
+
+  modules: [
+      // Cloudflare Analytics
+      [
+          'nuxt-cloudflare-analytics',
+          {
+              token: '07684a2c00274afeac7127bec28c3e3d',
+          }
+      ],
+      // SEO
+      '@nuxtjs/seo',
+      // TailwindCSS
+      [
+          '@nuxtjs/tailwindcss',
+          {
+              exposeConfig: true,
+          }
+      ],
+      // G Analytics
+      [
+          'nuxt-gtag',
+          {
+              id: 'G-1WY2RCYLML'
+          }
+      ],
+  ],
+
+  site: {
+      name: '/sβin.dev',
+      url: 'https://slashsbin.dev'
+  },
+
+  robots: {
+      credits: false,
+  },
+
+  sitemap: {
+      autoLastmod: true,
+      credits: false,
+  },
 })
